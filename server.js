8b9c8694a9cb5e5821b8a510d2306755f8b8f175
BIswanath Prasad Singh <biswanathprasadsingh9@gmail.com>
2021-05-26 20:01:52 +0530

    '26May2021'

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ var imagekit = new ImageKit({
 });
 // FOR IMAGEKIT AUTH
 
-const User = require('./routes/user');
 const ProductCategory = require('./routes/productcategory');
 const ProductSubCategory = require('./routes/productsubcategory');
 const Product = require('./routes/product');
@@ -52,7 +51,6 @@ app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
 });
 
-app.use('/api/user',User);
 app.use('/api/productcategory',ProductCategory);
 app.use('/api/productsubcategory',ProductSubCategory);
 app.use('/api/product',Product);
