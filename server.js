28409fccfb603adbbd76168be6431a9691985150
BIswanath Prasad Singh <biswanathprasadsingh9@gmail.com>
2021-05-26 19:55:31 +0530

    '26May2021'

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// FOR IMAGEKIT AUTH
+const ImageKit = require('imagekit');
+var imagekit = new ImageKit({
+    publicKey : "public_PT35bTumSZqcSo2PDGddDRBW5V8=",
+    privateKey : "private_0gua1/UMbXWRKqFvuKjVntd+Xw4=",
+    urlEndpoint : "https://ik.imagekit.io/aquariumstore"
+});
+// FOR IMAGEKIT AUTH
 
 
 mongoose.connect('mongodb+srv://b21341995returns:gurubaba@123@aquastore.mkuty.mongodb.net/databaseaquastore?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology:true});
@@ -24,6 +34,11 @@ app.use(bodyParser.json());
 app.get('/', (req,res)=>{
     res.send('<h2>Hello World</h2>')
 })
+app.get('/imagekitauth', function (req, res) {
+  const result = imagekit.getAuthenticationParameters();
+  res.send(result);
+});
+
 
 
 const PORT = process.env.port || 5000;
