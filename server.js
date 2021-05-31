d69b84ad3a2de3aa9b018e5d0429c7fd5e68857f
BIswanath Prasad Singh <biswanathprasadsingh9@gmail.com>
2021-05-31 21:06:06 +0530

    '31May2021'

--- server.js
+++ server.js
@@ -42,6 +42,14 @@ app.use(bodyParser.json());
 app.use(cors())
 
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+
 app.get('/', (req,res)=>{
     res.send('<h2>Hello World</h2>')
 })
